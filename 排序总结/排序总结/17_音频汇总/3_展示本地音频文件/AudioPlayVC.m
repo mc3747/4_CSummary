42aefc18a8186df52ba953353b73dbad1ce2df6f
mc3747 <mc3747@126.com>
2018-10-26 18:03:58 +0800

    添加音频

---
+++ 排序总结/排序总结/17_音频汇总/3_展示本地音频文件/AudioPlayVC.m
@@ -0,0 +1,32 @@
+//
+//  AudioPlayVC.m
+//  排序总结
+//
+//  Created by gjfax on 2018/10/26.
+//  Copyright © 2018年 macheng. All rights reserved.
+//
+
+#import "AudioPlayVC.h"
+
+@interface AudioPlayVC ()
+
+@end
+
+@implementation AudioPlayVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
