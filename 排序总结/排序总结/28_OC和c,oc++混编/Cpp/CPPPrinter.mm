b244ae9db3c6191c1ac2d1ff478a37cbf3e3f603
mc3747 <mc3747@126.com>
2020-03-11 17:31:53 +0800

    混编

---
+++ 排序总结/排序总结/28_OC和c,oc++混编/Cpp/CPPPrinter.mm
@@ -0,0 +1,31 @@
+//
+//  CPPPrinter.cpp
+//  CrossCompileDemo
+//
+//  Created by SamirChen on 12/26/16.
+//  Copyright © 2016 CX. All rights reserved.
+//
+
+#include "CPPPrinter.h"
+
+
+using namespace std;
+
+namespace CPPPrinter {
+    void sayHello() {
+        printf("%s, %s, %d\n", __FILE__, __func__, __LINE__);
+    }
+    
+    CPPPrinter::CPPPrinter() {
+        printf("%s, %s, %d\n", __FILE__, __func__, __LINE__);
+    }
+    
+    CPPPrinter::~CPPPrinter() {
+        printf("%s, %s, %d\n", __FILE__, __func__, __LINE__);
+    }
+    
+    void CPPPrinter::sayHello() {
+        printf("%s, %s, %d\n", __FILE__, __func__, __LINE__);
+    }
+    
+}
